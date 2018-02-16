9157a680382991f685654f6ce27fbfe5615acc43
Cyadenz <ugo.perniceni@hotmail.fr>
2018-02-16 22:48:48 +0100

    ajout theme

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,13 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/laravel', function () {
     return view('welcome');
 });
 
+Route::get('/', function () {
+    return view('index');
+});
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
